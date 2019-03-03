08e2b44be469d60efef515e8603303dccc830cf2
Pierre Böhm <pierre.boehm@bitingbit.com>
2019-03-03 15:15:06 +0100

    fix app-crash if app goes into background

--- app/src/main/java/org/pb/android/geomap3d/widget/Widget.java
+++ app/src/main/java/org/pb/android/geomap3d/widget/Widget.java
@@ -1,18 +1,20 @@
 package org.pb.android.geomap3d.widget;
 
+import android.location.Location;
+import android.os.Parcelable;
 import android.view.MotionEvent;
-
-import java.io.Serializable;
 
 import javax.microedition.khronos.opengles.GL10;
 
-public abstract class Widget implements Serializable {
+public abstract class Widget implements Parcelable {
 
     public abstract void draw(GL10 gl);
 
-    public abstract void updateTouch(MotionEvent motioEvent);
+    public abstract void updateTouch(MotionEvent motionEvent);
 
     public abstract void updateDeviceRotation(float azimuth);
+
+    public abstract void updateDeviceLocation(Location location);
 
     public abstract void initWidget(WidgetConfiguration widgetConfiguration);
 
