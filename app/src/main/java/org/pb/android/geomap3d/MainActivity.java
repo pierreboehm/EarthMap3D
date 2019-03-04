427cdfd868ce659bc0fb6233530e4731141ee2fa
Pierre Böhm <pierre.boehm@bitingbit.com>
2019-03-04 14:05:54 +0100

    implement location permission check / request

--- app/src/main/java/org/pb/android/geomap3d/MainActivity.java
+++ app/src/main/java/org/pb/android/geomap3d/MainActivity.java
@@ -1,8 +1,13 @@
 package org.pb.android.geomap3d;
 
+import android.Manifest;
 import android.app.ActivityManager;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 
 import org.androidannotations.annotations.AfterViews;
@@ -25,6 +30,8 @@ import org.pb.android.geomap3d.widget.WidgetManager;
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST_CODE = 7321;
+
     @SystemService
     ActivityManager activityManager;
 
@@ -36,9 +43,8 @@ public class MainActivity extends AppCompatActivity {
 
     @AfterViews
     public void init() {
-        if (widgetManager.getWidget() == null) {
-            LoadingFragment loadingFragment = LoadingFragment_.builder().build();
-            setFragment(loadingFragment, LoadingFragment.TAG);
+        if (checkPermissions()) {
+            initWidgetAfterPermissionCheck();
         }
     }
 
@@ -56,6 +62,27 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            boolean allPermissionsGranted = true;
+
+            for (int index = 0; index < permissions.length; index++) {
+                int result = grantResults[index];
+
+                if (result != PermissionChecker.PERMISSION_GRANTED) {
+                    allPermissionsGranted = false;
+                }
+            }
+
+            if (allPermissionsGranted) {
+                initWidgetAfterPermissionCheck();
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onEvent(Events.WidgetReady event) {
         TerrainFragment terrainFragment = TerrainFragment_.builder().widget(widgetManager.getWidget()).build();
@@ -66,6 +93,29 @@ public class MainActivity extends AppCompatActivity {
     public void onEvent(Events.FragmentLoaded event) {
         if (event.getTag().equals(LoadingFragment.TAG)) {
             setupTerrainWidget();
+        }
+    }
+
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                &&  ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            requestPermissions();
+            return false;
+        }
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSION_REQUEST_CODE);
+    }
+
+    private void initWidgetAfterPermissionCheck() {
+        if (widgetManager.getWidget() == null) {
+            LoadingFragment loadingFragment = LoadingFragment_.builder().build();
+            setFragment(loadingFragment, LoadingFragment.TAG);
         }
     }
 
