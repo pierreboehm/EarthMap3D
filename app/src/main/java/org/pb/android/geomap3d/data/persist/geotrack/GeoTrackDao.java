44cbbf3bb58a6cd93ccbedb4415ac5f422e3990e
pierreboehm <pierre.boehm@web.de>
2020-01-22 13:07:14 +0100

    commit test

--- app/src/main/java/org/pb/android/geomap3d/data/persist/geotrack/GeoTrackDao.java
+++ app/src/main/java/org/pb/android/geomap3d/data/persist/geotrack/GeoTrackDao.java
@@ -30,4 +30,9 @@ public class GeoTrackDao {
     public void deleteTracksOfArea(String areaName) {
         SQLite.delete().from(GeoTrack.class).where(GeoTrack_Table.areaName.eq(areaName));
     }
+
+    private void doMagic() {
+        // just a svn commit test
+    }
+
 }
