0dfd179049dcc5d26de7830d370652ace389d8ad
pierreboehm <pierre.boehm@web.de>
2020-08-06 13:29:13 +0200

    add bionic eye horizont graphic

--- app/src/main/java/org/pb/android/geomap3d/view/BionicEyeView.java
+++ app/src/main/java/org/pb/android/geomap3d/view/BionicEyeView.java
@@ -2,16 +2,21 @@ package org.pb.android.geomap3d.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EViewGroup;
+import org.androidannotations.annotations.ViewById;
 import org.pb.android.geomap3d.R;
 
 @EViewGroup(R.layout.bionic_eye_view)
 public class BionicEyeView extends RelativeLayout {
+
+    @ViewById(R.id.ivHorizont)
+    ImageView ivHorizont;
 
     public BionicEyeView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
